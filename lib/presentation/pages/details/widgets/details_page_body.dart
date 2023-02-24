ce37e800fffaaaf9c044944deaf9b5d6d9daeebc
kamilksi <kamilksiazek002@gmail.com>
2023-02-24 22:22:48 +0100

    finish detail page

--- lib/presentation/pages/details/widgets/details_page_body.dart
+++ lib/presentation/pages/details/widgets/details_page_body.dart
@@ -2,20 +2,37 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/presentation/pages/details/widgets/buttons_row.dart';
 import 'package:netflix_clone/presentation/pages/details/widgets/detail_header.dart';
-import 'package:netflix_clone/presentation/pages/details/widgets/images_row.dart';
 import 'package:netflix_clone/presentation/pages/details/widgets/overview_widget.dart';
 
 class DetailsPageBody extends StatelessWidget {
-  const DetailsPageBody({Key? key}) : super(key: key);
+  final String? overview;
+  final String? title;
+  final double? voteAverage;
+  final String? releaseDate;
+  final String? imgUrl;
+  const DetailsPageBody({
+    Key? key,
+    this.overview,
+    this.title,
+    this.releaseDate,
+    this.voteAverage,
+    this.imgUrl,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
-        children: const [
-          DetailHeader(title: "Black Panther", whenRelease: "(2023)"),
-          ButtonsRow(userScore: "100"),
-          OverviewWidget(),
+        children: [
+          DetailHeader(
+            title: title,
+            whenRelease: releaseDate,
+            imgUrl: imgUrl,
+          ),
+          ButtonsRow(userScore: voteAverage ?? 0),
+          OverviewWidget(
+            description: overview,
+          ),
         ],
       ),
     );
